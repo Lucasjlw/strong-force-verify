f74d1f777f3249f63d42e5741882cb63392f72e3
Lucasjlw <lucasjlw@gmail.com>
2023-12-31 17:47:02 -0600

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'strong-force-verify'
+import 'strong-force-verify/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
