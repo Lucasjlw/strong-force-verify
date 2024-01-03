0cdc81a2f227c40bc3cb635363a7c8dc7227d20a
Lucasjlw <lucasjlw@gmail.com>
2024-01-02 21:13:00 -0600

    Update import statement in index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-import { PhoneForm } from "./components";
+import { PhoneForm } from "./client";
 
 export { PhoneForm };
